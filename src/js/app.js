309aafd7aa872cc41e0b1189150b10d7eaf4471a
albert_u <albert.u1994@gmail.com>
2019-04-21 20:02:40 +0300

    1,2,3,4 ex. was done

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
-import { ZodiacSign, normolized } from "./lib.js";
-const person = new ZodiacSign(20, 1);
-console.log(person.showZodiacSign(person));
-const data = [12,3,0,234,45,33,4,5,4,65,12];
+import { zodiacSign, normolized, uniq, isSorted } from "./lib.js";
+console.log(zodiacSign(4,12));
+const data = [2,15,28,100,2,0,15,1];
 console.log(normolized(data, 2));
+console.log('data is ' + data);
+
+const data2 = [2,15,28,100,2,0,15,1];
+console.log(uniq(data2));
+console.log(data2);
+const data3 = [1, 8, 18, 50, 150 , 200 , 205, 1200];
+console.log(isSorted(data3));
--- src/js/lib.js
+++ src/js/lib.js
@@ -1,6 +1,6 @@
-export function ZodiacSign(date, month){
-    this.date = date;
-    this.month = month;
+export function zodiacSign(date, month){
+    // this.date = date;
+    // this.month = month;
     var signs = [
         {name:'козерог', month:1, date:20},
         {name:'водолей', month:2, date:20},
@@ -13,24 +13,59 @@ export function ZodiacSign(date, month){
         {name:'дева',    month:9, date:23},
         {name:'весы',    month:10,date:23},
         {name:'скорпион',month:11,date:22},
-        {name:'стрелец', month:12,date:21},
-        {name:'козерог', month:13,date:20}
+        {name:'стрелец', month:12,date:21}
         ];
-    this.showZodiacSign = function (target){
-        if(signs[target.month-1].date <= target.date ){
-            return signs[target.month].name        
-        } else { 
-            return signs[target.month - 1].name;
-        }
-    }   
+    if (signs[month % signs.length].date < date){
+        return signs[month].name;
+    } else {
+        return signs[month - 1].name;
+    }
+    // this.showZodiacSign = function (target){
+    //     if(signs[target.month-1].date <= target.date ){
+    //         return signs[target.month].name        
+    //     } else { 
+    //         return signs[target.month - 1].name;
+    //     }
+    // }   
 }
 
-////////////////////////////////////////
+/////////////////// задача 2 /////////////////////
 export function normolized(arr, n){
-    // function compareNumeric(a, b) {
-    //     if (a > b) return 1;
-    //     if (a < b) return -1;
-    // }
-    
+    const newArr = arr.slice().sort(function(a,b){
+       return a - b;
+    });
+    console.log('newARR ' + newArr)
+    const removedElem = newArr.splice(n, newArr.length - (2*n));
+    return removedElem;
+}
 
+
+
+///////////////////// задача 3 /////////////////////////
+export function uniq(data){
+    let sorted_arr = data.slice().sort(function(a,b){
+        return a - b;
+     });
+    let results = [];
+    for (let i = 0; i <= sorted_arr.length; i++) {
+        if (sorted_arr[i + 1] !== sorted_arr[i]) {
+            results.push(sorted_arr[i]);
+        }
+    }
+    return results;
+}
+
+
+//////////////////////// задача 4 ////////////////////
+
+export function isSorted(data){
+    let result = true;
+    for (let i = 0; i < data.length - 1; i++){
+        if (data[i]<data[i+1]){
+            continue;
+        } else {
+            return false
+        } 
+    }
+    return result;
 }
